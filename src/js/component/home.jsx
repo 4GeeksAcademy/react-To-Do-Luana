a83a36c651075878af500d6789082fd1e70b5ffe
Luana Tajes <lmluana27@gmail.com>
2024-10-28 22:17:45 +0000

    To Do list Terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React , {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const[tarea,setTarea] = useState("")
+	const [listaTareas, setListaTareas] = useState(["Aprender React", "Cocinar", "Estudiar"])
+	const agregarTarea = (evento)=>{
+		if(evento.key === "Enter"){
+			setListaTareas([...listaTareas, tarea])
+			setTarea("")
+		}
+	}
+	const borrarTarea = (evento, index) => {
+		evento.preventDefault()
+		let listaFiltrada= listaTareas.filter((item, id ) => {
+			return(id != index)
+		})
+		setListaTareas(listaFiltrada)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container">
+			<h1 className="text-center mt-5">To Do List!</h1>
+			<input type="text" className="form-control" 
+			value = {tarea}
+			onChange={(evento)=> setTarea(evento.target.value)}
+			onKeyDown ={agregarTarea}
+			/>
+			<ul className="list-group mt-3">
+				{listaTareas.map((item,index)=>(
+					<li className="list-group-item" key={index}> 
+					{item} 
+					<i className="fa fa-trash float-end fs-4 text-danger icono-oculto" onClick = {(evento) => borrarTarea(evento,index)}></i>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
